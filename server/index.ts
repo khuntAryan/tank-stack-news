e9f73620628ca8511771ac4092f16ea7dbee4c6d
Aryan Khunt <151055762+khuntAryan@users.noreply.github.com>
2025-04-12 20:45:28 +0530

    HONO error handling

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import type { ErrorResponse } from "@/shared/types";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 
@@ -11,8 +12,18 @@ app.onError((err,c)=>{
   if(err instanceof HTTPException){
     const errResponse = 
     err.res ?? 
-    c.json
+    c.json<ErrorResponse>({
+      success: false,
+      error: err.message,
+      isFormError: err.cause && typeof err.cause === "object" && "form" in err.cause ? err.cause.form === true:false,
+    },err.status);
+    return errResponse;
   }
+
+  return c.json<ErrorResponse>({
+    success: false,
+    error: process.env.NODE_ENV === "production" ? "Internal Server Error" : (err.stack ?? err.message)
+  },500)
 })
 
 export default app;
