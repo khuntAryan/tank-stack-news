c7369ba9993d52d1394f9da45106b1a0145bffb7
Aryan Khunt <151055762+khuntAryan@users.noreply.github.com>
2025-04-12 20:25:01 +0530

    Exporting the global types

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,18 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.onError((err,c)=>{
+  if(err instanceof HTTPException){
+    const errResponse = 
+    err.res ?? 
+    c.json
+  }
+})
 
 export default app;
